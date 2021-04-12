cdcca85f7da7dbb125ee5eb4f1c7d6106aab9849
skinnychenpi <117010037@link.cuhk.edu.cn>
2021-04-12 10:16:44 +0800

    Ex3 Task 3

--- L4/ex3/ex3.c
+++ L4/ex3/ex3.c
@@ -188,7 +188,7 @@ void addPartitionAtLevel( unsigned int lvl, unsigned int offset )
  *********************************************************/
 {
     // Find Buddy
-    int buddyOffset = buddyOf(offset + hmi.base, lvl) - hmi.base;
+    int buddyOffset = buddyOf(offset + hmi.base, lvl);
     printf("The buddy OFFSET IS : %d \n",buddyOffset);
     
     // Insertion
